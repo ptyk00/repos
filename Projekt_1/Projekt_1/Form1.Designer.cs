011455925bebc3f3421a0b95502ef3d5740438c7
Patryk <ptyk859@wp.pl>
2020-05-28 17:27:21 +0200

    Projekt 1
    Projekt 1

---
+++ Projekt_1/Projekt_1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Projekt_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IDProduktu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NazwaProduktu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDKategorii = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IlośćJednostkowa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CenaSprzedaży = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CenaZakupu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StanMagazynu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IDProduktu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "NazwaProduktu";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "IDKategori";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "IlośćJednostkowa";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "CenaSprzedaży";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(111, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "CenaZakupu";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(111, 150);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "StanMagazynu";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(111, 176);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDProduktu,
+            this.NazwaProduktu,
+            this.IDKategorii,
+            this.IlośćJednostkowa,
+            this.CenaSprzedaży,
+            this.CenaZakupu,
+            this.StanMagazynu});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Usun";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // IDProduktu
+            // 
+            this.IDProduktu.DataPropertyName = "IDProduktu";
+            this.IDProduktu.HeaderText = "IDProduktu";
+            this.IDProduktu.Name = "IDProduktu";
+            // 
+            // NazwaProduktu
+            // 
+            this.NazwaProduktu.DataPropertyName = "NazwaProduktu";
+            this.NazwaProduktu.HeaderText = "NazwaProduktu";
+            this.NazwaProduktu.Name = "NazwaProduktu";
+            // 
+            // IDKategorii
+            // 
+            this.IDKategorii.DataPropertyName = "IDKategori";
+            this.IDKategorii.HeaderText = "IDKategorii";
+            this.IDKategorii.Name = "IDKategorii";
+            // 
+            // IlośćJednostkowa
+            // 
+            this.IlośćJednostkowa.DataPropertyName = "IlośćJedkostkowa";
+            this.IlośćJednostkowa.HeaderText = "IlośćJednostkowa";
+            this.IlośćJednostkowa.Name = "IlośćJednostkowa";
+            // 
+            // CenaSprzedaży
+            // 
+            this.CenaSprzedaży.DataPropertyName = "CenaSprzedaży";
+            this.CenaSprzedaży.HeaderText = "CenaSprzedaży";
+            this.CenaSprzedaży.Name = "CenaSprzedaży";
+            // 
+            // CenaZakupu
+            // 
+            this.CenaZakupu.DataPropertyName = "CenaZakupu";
+            this.CenaZakupu.HeaderText = "CenaZakupu";
+            this.CenaZakupu.Name = "CenaZakupu";
+            // 
+            // StanMagazynu
+            // 
+            this.StanMagazynu.DataPropertyName = "StanMagazynu";
+            this.StanMagazynu.HeaderText = "StanMagazynu";
+            this.StanMagazynu.Name = "StanMagazynu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDProduktu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NazwaProduktu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDKategorii;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IlośćJednostkowa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CenaSprzedaży;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CenaZakupu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StanMagazynu;
+    }
+}
+
---
+++ Projekt_1/Projekt_1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_1
+{
+    public partial class Form1 : Form
+    {
+        Produkty produkty = new Produkty();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Dane()
+        {
+            using (BDII_projEntities db = new BDII_projEntities())
+            {
+                dataGridView1.DataSource = db.Produkty.ToList<Produkty>();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           Dane();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            produkty.IDProduktu       = Convert.ToInt32(textBox1.Text.Trim());
+            produkty.NazwaProduktu    = textBox2.Text.Trim();
+            produkty.IDKategori       = Convert.ToInt32(textBox3.Text.Trim());
+            produkty.IlośćJedkostkowa = Convert.ToInt32(textBox4.Text.Trim());
+            produkty.CenaSprzedaży    = Convert.ToDecimal(textBox5.Text.Trim());
+            produkty.CenaZakupu       = Convert.ToDecimal(textBox6.Text.Trim());
+            produkty.StanMagazynu     = Convert.ToInt32(textBox7.Text.Trim());
+
+            using (BDII_projEntities db = new BDII_projEntities())
+            {
+                db.Produkty.Add(produkty);
+                db.SaveChanges();
+            }
+            MessageBox.Show("Dodwanie powiodło się", "dodawanie");
+            Dane();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            using (BDII_projEntities db = new BDII_projEntities())
+            {
+                var k = db.Entry(produkty);
+                if (k.State == EntityState.Detached)
+                    db.Produkty.Attach(produkty); 
+                db.Produkty.Remove(produkty);
+                db.SaveChanges();
+                Dane();
+            }
+         
+            MessageBox.Show("Usunięto wiersz", "usuwanie");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            produkty.IDProduktu = Convert.ToInt32(dataGridView1.CurrentRow.Cells["IDProduktu"].Value);
+            using(BDII_projEntities db = new BDII_projEntities())
+            {
+                produkty = db.Produkty.Where(x => x.IDProduktu == produkty.IDProduktu).FirstOrDefault();
+
+                textBox1.Text = Convert.ToString(produkty.IDProduktu);
+                textBox2.Text = produkty.NazwaProduktu;
+                textBox3.Text = Convert.ToString(produkty.IDKategori);
+                textBox4.Text = Convert.ToString(produkty.IlośćJedkostkowa);
+                textBox5.Text = Convert.ToString(produkty.CenaSprzedaży);
+                textBox6.Text = Convert.ToString(produkty.CenaZakupu);
+                textBox7.Text = Convert.ToString(produkty.StanMagazynu);
+            }
+        }
+    }
+}
